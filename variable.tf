3245bfb2fae807439eabe5e5005f9163f57ccee7
soubhagya11 <168725707+soubhagya11@users.noreply.github.com>
2024-08-29 06:48:22 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,21 @@
+variable "ami" {
+    description = "ami for main file"
+    type = string
+    default = ""
+
+
+}
+
+variable "instance_type" {
+    description = "instance type fro main file"
+    type = string
+    default = ""
+
+}
+
+variable "key_name" {
+    description = "keypair name for main file"
+    type = string
+    default = ""
+
+}
